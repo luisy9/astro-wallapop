c50d554580772a50e5af84b6d5b806e43ae1148a
luisy9 <luisdeharo4@gmail.com>
2024-01-25 15:36:32 +0100

    astro + tailwind

---
+++ src/utils/data.js
@@ -0,0 +1,217 @@
+export const data = [
+  {
+    Nombre: 'iphone 14',
+    Precio: '630€',
+    imagen:
+      'https://cdn.wallapop.com/images/10420/g5/c0/__/c10420p976417303/i4721862858.jpg?pictureSize=W640',
+  },
+  {
+    Nombre: 'iphone',
+    Precio: '380€',
+    imagen:
+      'https://cdn.wallapop.com/images/10420/g5/b1/__/c10420p976371422/i4721656839.jpg?pictureSize=W640',
+  },
+  {
+    Nombre: 'Iphone XR',
+    Precio: '115€',
+    imagen:
+      'https://cdn.wallapop.com/images/10420/g5/c7/__/c10420p976426801/i4721911862.jpg?pictureSize=W640',
+  },
+  {
+    Nombre: 'Iphone 11',
+    Precio: '350€',
+    imagen:
+      'https://cdn.wallapop.com/images/10420/g5/c4/__/c10420p976422274/i4721888843.jpg?pictureSize=W640',
+  },
+  {
+    Nombre: 'iPhone X',
+    Precio: '50€',
+    imagen:
+      'https://cdn.wallapop.com/images/10420/g5/c4/__/c10420p976422274/i4721888966.jpg?pictureSize=W640',
+  },
+  {
+    Nombre: 'Iphone 12',
+    Precio: '280€',
+    imagen:
+      'https://cdn.wallapop.com/images/10420/g5/bs/__/c10420p976407508/i4721820400.jpg?pictureSize=W640',
+  },
+  {
+    Nombre: 'iphone 12',
+    Precio: '320€',
+    imagen:
+      'https://cdn.wallapop.com/images/10420/g5/b9/__/c10420p976382019/i4721704851.jpg?pictureSize=W640',
+  },
+  {
+    Nombre: 'iPhone 7🤍',
+    Precio: '85€',
+    imagen:
+      'https://cdn.wallapop.com/images/10420/g5/bu/__/c10420p976409786/i4721829719.jpg?pictureSize=W640',
+  },
+  {
+    Nombre: 'Iphone 15',
+    Precio: '850€',
+    imagen:
+      'https://cdn.wallapop.com/images/10420/g5/c3/__/c10420p976420932/i4721881591.jpg?pictureSize=W640',
+  },
+  {
+    Nombre: 'iphone 13 promax',
+    Precio: '600€',
+    imagen:
+      'https://cdn.wallapop.com/images/10420/g5/av/__/c10420p976364659/i4721622685.jpg?pictureSize=W640',
+  },
+  {
+    Nombre: 'iphone XR 11 iphone 13 iphone 12',
+    Precio: '199€',
+    imagen:
+      'https://cdn.wallapop.com/images/10420/g5/aq/__/c10420p976357259/i4721586153.jpg?pictureSize=W640',
+  },
+  {
+    Nombre: 'iPhone 12 Pro',
+    Precio: '0€',
+    imagen:
+      'https://cdn.wallapop.com/images/10420/g5/bo/__/c10420p976401603/i4721794968.jpg?pictureSize=W640',
+  },
+  {
+    Nombre: 'iPhone 12',
+    Precio: '279€',
+    imagen:
+      'https://cdn.wallapop.com/images/10420/g5/bp/__/c10420p976403351/i4721803269.jpg?pictureSize=W640',
+  },
+  {
+    Nombre: 'IPhone 11',
+    Precio: '250€',
+    imagen:
+      'https://cdn.wallapop.com/images/10420/g5/az/__/c10420p976369290/i4721646389.jpg?pictureSize=W640',
+  },
+  {
+    Nombre: 'iPhone 13 100%🔋',
+    Precio: '460€',
+    imagen:
+      'https://cdn.wallapop.com/images/10420/g5/bg/__/c10420p976391639/i4721749303.jpg?pictureSize=W640',
+  },
+  {
+    Nombre: 'iPhone X 🔋100%🔋',
+    Precio: '170€',
+    imagen:
+      'https://cdn.wallapop.com/images/10420/g5/bh/__/c10420p976393098/i4721756150.jpg?pictureSize=W640',
+  },
+  {
+    Nombre: 'Iphone 11',
+    Precio: '220€',
+    imagen:
+      'https://cdn.wallapop.com/images/10420/g5/be/__/c10420p976389140/i4721737961.jpg?pictureSize=W640',
+  },
+  {
+    Nombre: 'iphone 7 plus',
+    Precio: '160€',
+    imagen:
+      'https://cdn.wallapop.com/images/10420/g5/be/__/c10420p976388999/i4721737381.jpg?pictureSize=W640',
+  },
+  {
+    Nombre: 'iPhone 12 mini',
+    Precio: '450€',
+    imagen:
+      'https://cdn.wallapop.com/images/10420/g5/bp/__/c10420p976402611/i4721800225.jpg?pictureSize=W640',
+  },
+  {
+    Nombre: 'iPhone 14 Red',
+    Precio: '920€',
+    imagen:
+      'https://cdn.wallapop.com/images/10420/g5/ab/__/c10420p976338146/i4721494086.jpg?pictureSize=W640',
+  },
+  {
+    Nombre: 'iPhone 12 mini',
+    Precio: '350€',
+    imagen:
+      'https://cdn.wallapop.com/images/10420/g5/ad/__/c10420p976340705/i4721509987.jpg?pictureSize=W640',
+  },
+  {
+    Nombre: 'Iphone 12 pro',
+    Precio: '385€',
+    imagen:
+      'https://cdn.wallapop.com/images/10420/g5/bv/__/c10420p976410366/i4721832567.jpg?pictureSize=W640',
+  },
+  {
+    Nombre: 'iphone 14',
+    Precio: '630€',
+    imagen:
+      'https://cdn.wallapop.com/images/10420/g4/4d/__/c10420p974381824/i4711595753.jpg?pictureSize=W640',
+  },
+  {
+    Nombre: 'IPHONE 11',
+    Precio: '240€',
+    imagen:
+      'https://cdn.wallapop.com/images/10420/g5/a9/__/c10420p976336104/i4721484690.jpg?pictureSize=W640',
+  },
+  {
+    Nombre: 'Iphone 11 128gb',
+    Precio: '250€',
+    imagen:
+      'https://cdn.wallapop.com/images/10420/g5/bv/__/c10420p976410658/i4721833843.jpg?pictureSize=W640',
+  },
+  {
+    Nombre: 'Iphone XR, rojo',
+    Precio: '125€',
+    imagen:
+      'https://cdn.wallapop.com/images/10420/g5/bu/__/c10420p976409124/i4721827003.jpg?pictureSize=W640',
+  },
+  {
+    Nombre: 'Iphone 11',
+    Precio: '300€',
+    imagen:
+      'https://cdn.wallapop.com/images/10420/g5/b2/__/c10420p976373016/i4721664053.jpg?pictureSize=W640',
+  },
+  {
+    Nombre: 'IPHONE 12 (64 GB)',
+    Precio: '399€',
+    imagen:
+      'https://cdn.wallapop.com/images/10420/g2/y1/__/c10420p972405856/i4701783136.jpg?pictureSize=W640',
+  },
+  {
+    Nombre: 'IPHONE 13 (128 GB)',
+    Precio: '599€',
+    imagen:
+      'https://cdn.wallapop.com/images/10420/g5/a3/__/c10420p976328479/i4721449556.jpg?pictureSize=W640',
+  },
+  {
+    Nombre: 'iphone 11 Negro',
+    Precio: '240€',
+    imagen:
+      'https://cdn.wallapop.com/images/10420/g5/bq/__/c10420p976404932/i4721809951.jpg?pictureSize=W640',
+  },
+  {
+    Nombre: 'IPHONE 13',
+    Precio: '500€',
+    imagen:
+      'https://cdn.wallapop.com/images/10420/g5/bs/__/c10420p976406755/i4721817388.jpg?pictureSize=W640',
+  },
+  {
+    Nombre: 'Iphone 11',
+    Precio: '250€',
+    imagen:
+      'https://cdn.wallapop.com/images/10420/g5/c0/__/c10420p976417942/i4721865990.jpg?pictureSize=W640',
+  },
+  {
+    Nombre: 'Iphone 14 128GB',
+    Precio: '680€',
+    imagen:
+      'https://cdn.wallapop.com/images/10420/g5/bn/__/c10420p976400760/i4721789827.jpg?pictureSize=W640',
+  },
+  {
+    Nombre: 'Iphone 15 pro',
+    Precio: '960€',
+    imagen:
+      'https://cdn.wallapop.com/images/10420/g5/9x/__/c10420p976320347/i4721412004.jpg?pictureSize=W640',
+  },
+  {
+    Nombre: 'iphone XR 256GB',
+    Precio: '235€',
+    imagen:
+      'https://cdn.wallapop.com/images/10420/g5/au/__/c10420p976363048/i4721614786.jpg?pictureSize=W640',
+  },
+  { Nombre: 'iphone 12 negro espacial', Precio: '365€', imagen: null },
+  { Nombre: 'Iphone XR 64GB', Precio: '280€', imagen: null },
+  { Nombre: 'Caja Iphone', Precio: '7€', imagen: null },
+  { Nombre: 'Iphone 14 pro max', Precio: '930€', imagen: null },
+  { Nombre: 'iphone XR', Precio: '125€', imagen: null },
+];
